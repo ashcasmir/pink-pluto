3ec7f394660a3a5367c4219784bd73d3e1104a77
Stackbit Code Editor <projects@stackbit.com>
2021-04-06 14:17:22 +0000

    home.js: changed

--- src/templates/home.js
+++ src/templates/home.js
@@ -33,13 +33,12 @@ export default class Home extends React.Component {
                   <div className="intro-cta">
                     {_.map(_.get(this.props, 'pageContext.frontmatter.intro_actions', null), (action, action_idx) => (
                     <a href='mailto:ashcasmir@gmail.com'></a>
-                        <Link key={action_idx} to={withPrefix(_.get(action, 'url', null))}
+                    <Link key={action_idx} to={withPrefix(_.get(action, 'url', null))}
                       {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
                       {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
                       className={classNames({'button': (_.get(action, 'style', null) === 'primary') || (_.get(action, 'style', null) === 'secondary'), 'button-secondary': _.get(action, 'style', null) === 'secondary'})}>
                       {_.get(action, 'label', null)}
-                        </Link>
-                    </a>
+                    </Link>
                     ))}
                   </div>
                   )}
