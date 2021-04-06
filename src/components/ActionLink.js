80b284f43bf6d18abe7e274a947b0c6f1ba8ce68
Stackbit Code Editor <projects@stackbit.com>
2021-04-06 14:21:38 +0000

    ActionLink.js: changed

--- src/components/ActionLink.js
+++ src/components/ActionLink.js
@@ -10,7 +10,8 @@ export default class ActionLink extends React.Component {
         let action_style = _.get(action, 'style', null) || 'link';
         let action_icon = _.get(action, 'icon_class', null) || 'dev';
         return (
-            <Link to={withPrefix(_.get(action, 'url', null))}
+            <center>
+                </center><Link to={withPrefix(_.get(action, 'url', null))}
               {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
               {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
               className={classNames({'button': action_style !== 'link', 'button-secondary': action_style === 'secondary', 'button-icon': action_style === 'icon'})}>
